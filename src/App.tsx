ea2e18d62f32bc23ab5856f3cc97b7674edbedfb
Calvin <calvinalvito19@gmail.com>
2024-07-28 20:42:30 +0700

    feature: Add bankstatementprovider in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import UnderMaintenance from "./pages/UnderMaintenance";
 import { AuthProvider } from "./contexts/AuthContext";
+import { BankStatementProvider } from "./contexts/BankStatementContext";
 import Beranda from "./pages/Beranda";
 import Administrasi from "./pages/Administrasi";
 import Email from "./pages/Email";
@@ -18,83 +19,85 @@ function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/maintenance" element={<UnderMaintenance />} />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Beranda />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/saldo-mutasi"
-            element={
-              <PrivateRoute>
-                <SaldoMutasi />
-              </PrivateRoute>
-            }
-          >
+        <BankStatementProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/maintenance" element={<UnderMaintenance />} />
             <Route
-              path="informasi-saldo-rekening"
+              path="/"
               element={
                 <PrivateRoute>
-                  <InfoSaldo />
+                  <Beranda />
                 </PrivateRoute>
               }
             />
             <Route
-              path="mutasi-rekening"
+              path="/saldo-mutasi"
               element={
                 <PrivateRoute>
-                  <InfoSaldo />
+                  <SaldoMutasi />
+                </PrivateRoute>
+              }
+            >
+              <Route
+                path="informasi-saldo-rekening"
+                element={
+                  <PrivateRoute>
+                    <InfoSaldo />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="mutasi-rekening"
+                element={
+                  <PrivateRoute>
+                    <InfoSaldo />
+                  </PrivateRoute>
+                }
+              />
+            </Route>
+            <Route
+              path="/transaksi"
+              element={
+                <PrivateRoute>
+                  <Transaksi />
                 </PrivateRoute>
               }
             />
-          </Route>
-          <Route
-            path="/transaksi"
-            element={
-              <PrivateRoute>
-                <Transaksi />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/informasi-lainnya"
-            element={
-              <PrivateRoute>
-                <InformasiLainya />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/administrasi"
-            element={
-              <PrivateRoute>
-                <Administrasi />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/e-mail"
-            element={
-              <PrivateRoute>
-                <Email />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profil"
-            element={
-              <PrivateRoute>
-                <Profil />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+            <Route
+              path="/informasi-lainnya"
+              element={
+                <PrivateRoute>
+                  <InformasiLainya />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/administrasi"
+              element={
+                <PrivateRoute>
+                  <Administrasi />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/e-mail"
+              element={
+                <PrivateRoute>
+                  <Email />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profil"
+              element={
+                <PrivateRoute>
+                  <Profil />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </BankStatementProvider>
       </AuthProvider>
     </Router>
   );
