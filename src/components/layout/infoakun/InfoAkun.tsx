ddd7ceaad0c46cece7eed874c4f7f800e7fb1c33
Calvin <calvinalvito19@gmail.com>
2024-08-01 16:55:53 +0700

    style: Update style in component

--- src/components/layout/infoakun/InfoAkun.tsx
+++ src/components/layout/infoakun/InfoAkun.tsx
@@ -8,24 +8,24 @@ const InfoAkun: React.FC = () => {
 
   return (
     <div className="flex flex-col gap-[20px]">
-      <h1 className="text-lg text-white font-bold">Informasi Akun</h1>
+      <h1 className="text-lg text-white font-bold" aria-label="informasi akun">Informasi Akun</h1>
       {!loginInfo || !bankStatement ? (
         <div className="text-white">No data available</div>
       ) : (
-        <div className="bg-primary-blue rounded-5 flex flex-col w-[400px] rounded-[20px] p-5 gap-[7px]">
+        <div className="bg-primary-light-blue flex flex-col w-[416px] rounded-[20px] p-[18px] gap-[7px]">
           <div className="flex gap-2">
-            <p className="w-[215px] text-white text-sm font-semibold">
+            <p className="w-[215px] text-primary-dark-blue text-sm font-semibold">
               Masa Berlaku Pin (hari)
             </p>
-            <p className="text-white text-sm font-semibold">
+            <p className="text-primary-dark-blue text-sm font-semibold">
               : {bankStatement.accountInfo.pinExpiredTimeLeft}
             </p>
           </div>
           <div className="flex gap-2">
-            <p className="w-[215px] text-white text-sm font-semibold">
+            <p className="w-[215px] text-primary-dark-blue text-sm font-semibold">
               Tanggal Terakhir Gagal Login
             </p>
-            <p className="text-white text-sm font-semibold">
+            <p className="text-primary-dark-blue text-sm font-semibold">
               :{" "}
               {new Date(
                 loginInfo.failedLoginAttempt.timestamp
@@ -33,10 +33,10 @@ const InfoAkun: React.FC = () => {
             </p>
           </div>
           <div className="flex gap-2">
-            <p className="w-[215px] text-white text-sm font-semibold">
+            <p className="w-[215px] text-primary-dark-blue text-sm font-semibold">
               Lokasi Terakhir Akun Terhubung
             </p>
-            <p className="text-white text-sm font-semibold">
+            <p className="text-primary-dark-blue text-sm font-semibold">
               : {loginInfo.lastSuccessfullLoginAttempt.location}
             </p>
           </div>
